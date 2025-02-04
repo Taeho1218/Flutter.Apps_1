12e328113c9e99654dda3e8d327ba0033697d0ef
Taeho1218 <true3d.j@gmail.com>
2025-02-04 23:34:35 +0900

    Update result.dart

--- result.dart
+++ result.dart
@@ -14,28 +14,37 @@ class FinalWeb extends StatelessWidget {
         //   title: Text("ResultPage"),
         // ),
         body: Container(
+          child: SingleChildScrollView(
           child:Column(
             children: [
               SizedBox(height: 50,),
               Image.asset('assets/images/img_mainSnake.png',
-                width: 500,
-                height: 500,
+                width: 300,
+                height:300,
                 fit: BoxFit.cover,), //닮은 꼴 이미지 등록
               SizedBox(height: 50,),
-              Row(
-                children: [
-                  Image.asset('assets/images/img_mainSnake.png',
-                    width: 50,
-                    height: 50,
-                    fit: BoxFit.cover,),//선택한 이미지들어갈 화면
-                  Text("닮은꼴 이름"),//닮은 꼴 이미지이름
-                ],
+              Center(
+                child:
+                Row(
+                  children: [
+                    SizedBox(width: 120,),
+
+                    Image.asset('assets/images/img_mainSnake.png',
+                      width: 50,
+                      height: 50,
+                      fit: BoxFit.cover,),//선택한 이미지들어갈 화면
+                    Text("닮은꼴 이름"),//닮은 꼴 이미지이름
+                  ],
+                ),
               ),
+
               SizedBox(height: 50,),
               Text("닮은꽇 해설")
 
             ],
           ) ,
+          ),
+
         ),
       ),
 
