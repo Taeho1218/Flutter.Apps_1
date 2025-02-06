aa424e3ea82b594ff515b345c2cd4575f2a8517c
Taeho1218 <true3d.j@gmail.com>
2025-02-07 01:22:58 +0900

    Update imageselect.dart

--- imageselect.dart
+++ imageselect.dart
@@ -1,9 +1,10 @@
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:project_taeyoung/main.dart';
 
 class ImageSelect extends StatefulWidget{
-  ImageSelect({Key? key}) : super(key: key);
+  const ImageSelect({super.key});
 
   @override
   State<ImageSelect> createState() => _ImageSelectState();
@@ -11,7 +12,7 @@ class ImageSelect extends StatefulWidget{
 
 class _ImageSelectState extends State<ImageSelect>{
 
-  XFile? image;
+  XFile? image; //이미지 선택 파일
 
   @override
   void initState() {
@@ -23,20 +24,48 @@ class _ImageSelectState extends State<ImageSelect>{
     super.dispose();
   }
 
-  void ImagePressed() async {
-    image = await ImagePicker().pickImage(source: ImageSource.gallery);
-    setState(() {
-
-    });
+  void imagePressed() async {
+   final pickedImage = await ImagePicker().pickImage(source: ImageSource.gallery); // await - 함수가 끝낳때 까지 기다림.
+   // 그 뒤 - 사용자의 갤러리 열어서 이미지 선택 후 파일 타입으로 저장
+    setState(() { // UI를 다시 그림.
+      print("페이지 넘어와서 이미지 선택 ");
+      if (pickedImage != null) {
+        image = pickedImage;
+        print("이미지가 선택되었습니다");
+        Navigator.pop(context, File(image as String));
+       Navigator.push(context,MaterialPageRoute(builder: (context) =>  MyHomePage()));
+      } else {
+          print("이미지 선택이 취소됌");
+        };
+       }
+      );
   }
 
   @override
   Widget build(BuildContext context) {
-    return(
-   ImagePressed(),
+    return Scaffold(
+          appBar: AppBar(title: Text('이미지 선택기')),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // 이미지 표시 영역
+                image != null
+                    ? Image.file(File(image!.path), width: 200, height: 200, fit: BoxFit.cover) //파일 타입으로 변환하여 이미지 로드
+                    : Text("이미지를 선택해주세요"),
+
+                SizedBox(height: 20),
+
+                // 이미지 선택 버튼
+                ElevatedButton(
+                  onPressed: imagePressed,
+                  child: Text("이미지 선택"),
+                ),
+              ],
+            ),
+          ),
+
     );
-
   }
-
 
 }
