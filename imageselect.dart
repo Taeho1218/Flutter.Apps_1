5dd4bed39f081e67939d6e3f9433edb0040e7cbb
Taeho1218 <true3d.j@gmail.com>
2025-06-11 01:24:31 +0900

    Update imageselect.dart

--- imageselect.dart
+++ imageselect.dart
@@ -1,7 +1,10 @@
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:heif_converter/heif_converter.dart';
 import 'package:project_taeyoung/main.dart';
+import 'package:exif/exif.dart';
+import 'package:image/image.dart' as img;
 
 class ImageSelect extends StatefulWidget{
   const ImageSelect({super.key});
@@ -24,6 +27,37 @@ class _ImageSelectState extends State<ImageSelect>{
     super.dispose();
   }
 
+  // Future<File> fixImageOrientation(File imageFile) async {
+  //   final bytes = await imageFile.readAsBytes();
+  //   final image = img.decodeImage(bytes);
+  //
+  //   if (image == null) return imageFile; // 디코딩 실패 시 원본 반환
+  //
+  //   final rotationDegree = await getImageRotationDegree(imageFile.path);
+  //
+  //   img.Image rotatedImage;
+  //   switch (rotationDegree) {
+  //     case 90:
+  //       rotatedImage = img.copyRotate(image, 90);
+  //       break;
+  //     case 180:
+  //       rotatedImage = img.copyRotate(image, 180);
+  //       break;
+  //     case 270:
+  //       rotatedImage = img.copyRotate(image, 270);
+  //       break;
+  //     default:
+  //       return imageFile;
+  //   }
+  //
+  //   // 새 파일로 저장 (임시 디렉토리)
+  //   final newPath = '${imageFile.parent.path}/fixed_${imageFile.uri.pathSegments.last}';
+  //   final newFile = File(newPath)..writeAsBytesSync(img.encodeJpg(rotatedImage));
+  //
+  //   return newFile;
+  // }
+
+
   void imagePressed() async {
     final pickedImage = await ImagePicker().pickImage(source: ImageSource.gallery); // await - 함수가 끝낳때 까지 기다림.
     print("페이지 넘어와서 이미지 선택 ");
@@ -31,9 +65,14 @@ class _ImageSelectState extends State<ImageSelect>{
       image = pickedImage;
       File imageFile = File(pickedImage.path);
       print("이미지가 선택되었습니다");
-      Navigator.pop(context,imageFile);
-      // 이미지를 받아왔을 떄 이미지를 보여주는 창을 만들고 거기에 이미지 넣어서 확인
-      // Navigator.push(context,MaterialPageRoute(builder: (context) =>  MyHomePage()));
+      print("$imageFile");
+      String target = 'heif';
+      if(imageFile.path.contains(target)){
+        String? jpgPath = await HeifConverter.convert(imageFile.path,format: 'png');
+        print(jpgPath);
+        imageFile = File(jpgPath!);
+      }Navigator.pop(context,imageFile);
+
       print(" 이미지가 메인으로 전송된 후 메인페이지로 돌아감");
     } else {
       print("이미지 선택이 취소됌");
