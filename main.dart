4f1a14e91f7121917afdbee43591bbe291814122
Taeho1218 <true3d.j@gmail.com>
2025-06-12 22:38:15 +0900

    Update main.dart

--- main.dart
+++ main.dart
@@ -14,6 +14,7 @@ import 'package:webview_flutter/webview_flutter.dart';
 import 'package:flutter/services.dart' show rootBundle;
 
 
+
 void main() {
   runApp(MyApp());
 }
@@ -41,7 +42,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-   File? result ;
+  File? result ;
   late final WebViewController _webViewController;
   String prediction = '분류 중...';
   late String name = prediction+'띠';
@@ -70,9 +71,12 @@ class _MyHomePageState extends State<MyHomePage> {
           }
         },
       );
-    _loadHtml();
+    // _loadHtml();
   }
 
+  // void waitResult() async{
+  //
+  // }
 
   void pickImage()async{
     final File? selectedImage = await Navigator.push(context, MaterialPageRoute(builder:(context)=> ImageSelect()),);
@@ -114,109 +118,119 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
 
-   @override
+  @override
 
 
-   Widget build(BuildContext context) {
-     return MaterialApp(
-         home: Scaffold(
-           body: Container(
-               child : SingleChildScrollView(
-                 child: Column(
-                   children: [
-                     SizedBox(
-                       width: 400, height: 100,
-                       child:                     Container(
-                         child:Center(
-                           child: Text("닮은꼴 운세 뽑기", style: TextStyle(fontSize: 40),),
-                         ),
-                       ),
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+          body: Container(
+              child : SingleChildScrollView(
+                child: Column(
+                  children: [
+                    SizedBox(
+                      width: 400, height: 100,
+                      child:                     Container(
+                        child:Center(
+                          child: Text("닮은꼴 운세 뽑기", style: TextStyle(fontSize: 40),),
+                        ),
+                      ),
 
-                     ),
-                     Column(
-                         mainAxisSize: MainAxisSize.min,
-                         children: [
-                           InkWell(
-                             onTap: (){
-                               print("이미지 선택페이지로 이동");
-                               pickImage();
-                             },
-                             child:   Stack(
-                               children: [
-                                 Image.asset(
-                                   'assets/images/img_mainSnake.jpg',
-                                   width: 350,
-                                   height: 450,
-                                   fit: BoxFit.cover,),
-                                 Positioned(
-                                     top:250 ,
-                                     left: 100,
-                                     child:
-                                     SizedBox(
-                                         width: 100, height: 100,
-                                         child:
-                                         result != null
-                                             ? CircleAvatar(
-                                             backgroundImage: FileImage(result!)
-                                         )
-                                             :CircleAvatar(
-                                             backgroundImage:AssetImage('assets/images/img_mainSnake.jpg')
-                                         )
+                    ),
+                    Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          InkWell(
+                            onTap: (){
+                              print("이미지 선택페이지로 이동");
+                              pickImage();
+                            },
+                            child:   Stack(
+                              children: [
+                                Image.asset(
+                                  'assets/images/img_mainSnake.jpg',
+                                  width: 350,
+                                  height: 450,
+                                  fit: BoxFit.cover,),
+                                Positioned(
+                                    top:250 ,
+                                    left: 100,
+                                    child:
+                                    SizedBox(
+                                        width: 100, height: 100,
+                                        child:
+                                          result != null
+                                            ? CircleAvatar(
+                                            backgroundImage: FileImage(result!)
+                                            )
+                                              :CircleAvatar(
+                                              backgroundImage:AssetImage('assets/images/img_mainSnake.jpg')
+                                          )
 
-                                     )
-                                 ),
-                               ],
-                             ),
-                           ),
-
-
-                           SizedBox(height: 20,),
-                           ElevatedButton(
-                               style: ElevatedButton.styleFrom(minimumSize: Size(200, 50) ),
-                               onPressed: (){
-                                 print("FianlWeb 으로 이동 ");
-                                 if (result != null) {
-                                   showDialog(context: context, builder: (BuildContext ctx){
-                                     return AlertDialog(
-                                       content: Container()
-                                       //Text('잠시만 기다려 주세요! 닮은 동물을 찾는 중입니다 . . .'),
-                                     );
-                                   });
-
-                                   _loadHtml();
-                                 }else{
-                                   showDialog(context: context, builder: (BuildContext ctx){
-                                     return AlertDialog(
-                                       content: Text('사진을 선택해 주세요'),
-                                       actions: [
-                                         Center(
-                                           child: FloatingActionButton(
-                                               child: Text('네'),
-                                               onPressed:(){
-                                                 Navigator.of(context).pop();
-                                               }
-                                           ),
-
-                                         )
-                                       ],
-                                     );
-                                   });
-                                 }
-                               },
-                               child: Text("결과 보기")),
-                         ]
-                     ),
+                                    )
+                                ),
+                              ],
+                            ),
+                          ),
 
 
-                   ],
-                 ),
-               )
+                          SizedBox(height: 20,),
+                          ElevatedButton(
+                              style: ElevatedButton.styleFrom(minimumSize: Size(200, 50) ),
+                              onPressed: (){
+                                print("FianlWeb 으로 이동 ");
+                                if (result != null) {
+                                  showDialog(context: context, builder: (BuildContext ctx){
+                                    return AlertDialog(
+                                      content: Container( height: 250,
+                                        child: Column(
+                                          children: [
+                                            Text('잠시만 기다려 주세요! 닮은 동물을 찾는 중입니다 . . .'),
+                                            SizedBox(height: 30),
+                                            Image.asset('assets/images/alert_snake.png',
+                                              width: 200,
+                                              height: 150,
+                                              fit: BoxFit.cover,)
+                                            ]
+                                        )
+                                      )
+                                    );
+                                  });
 
-           ),
-         )
-     );
+                                  _loadHtml();
+                                }else{
+                                  showDialog(context: context, builder: (BuildContext ctx){
+                                    return AlertDialog(
+                                      content: Text('사진을 선택해 주세요'),
+                                      actions: [
+                                        Center(
+                                          child: FloatingActionButton(
+                                              child: Text('네'),
+                                              onPressed:(){
+                                                Navigator.of(context).pop();
+                                              }
+                                          ),
+
+                                        )
+                                      ],
+                                    );
+                                  });
+                                }
+                              },
+                              child: Text("결과 보기")),
+                        ]
+                    ),
 
 
-   }
+                  ],
+                ),
+              )
+
+          ),
+        )
+    );
+
+
+  }
 
 }
